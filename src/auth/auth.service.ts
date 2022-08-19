6c9b2e98738a560ff49f9ef2800e638a911e943e
gisioraelvis <gisioraelvis20@gmail.com>
2022-08-19 09:58:56 +0300

    renamed access_token to accessToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
   async signIn(user: any): Promise<any> {
     const payload = { sub: user.id, email: user.email };
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken: this.jwtService.sign(payload),
     };
   }
 
